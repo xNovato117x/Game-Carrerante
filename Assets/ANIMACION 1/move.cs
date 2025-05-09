ce34892cd76ec8e4c5a5cb6063921af485b861dc
xNovato117x <henrirodrigo19@gmail.com>
2025-05-09 13:55:32 -0600

    vercion 0.8 09/05/2025

---
+++ Assets/ANIMACION 1/move.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour
+{
+    [SerializeField] float speed;
+    [SerializeField] int health = 5;
+
+    // Use this for initialization
+    void Start()
+    {
+        Invoke("Destroy", health);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+    }
+
+    void Autodestruction()
+    {
+        Destroy(gameObject);
+    }
+}
