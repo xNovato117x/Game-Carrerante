fd9b113a8b12016831a71dca5673b7266028dda7
xNovato117x <henrirodrigo19@gmail.com>
2025-05-10 20:15:50 -0600

    vercion 1.1

--- Assets/ANIMACION 1/move.cs
+++ Assets/ANIMACION 1/move.cs
@@ -2,21 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class move : MonoBehaviour{
-
+public class Move : MonoBehaviour
+{
     [SerializeField] float speed;
     [SerializeField] int health = 5;
 
     // Use this for initialization
-    void Start(){
-
+    void Start()
+    {
         Invoke("Autodestruction", health);
     }
 
     // Update is called once per frame
-    void Update(){
-
-        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+    void Update()
+    {
+        // Mover hacia atrás en el eje Z
+        transform.Translate(Vector3.back * speed * Time.deltaTime);
     }
 
     void Autodestruction()
--- Assets/ANIMACION 1/spawner.cs
+++ Assets/ANIMACION 1/spawner.cs
@@ -2,12 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class spawner : MonoBehaviour
+public class Spawner : MonoBehaviour
 {
     [SerializeField] float timeToSpawn = 1.5f;
-    public List<GameObject> obsPref;
+    public GameObject[] obsPref;
 
-    // Use this for initialization
+    // Definir posiciones para los carriles
+    private Vector3[] lanes = {
+        new Vector3(10, 0, 0), // Carril 1
+        new Vector3(20, 0, 0), // Carril 2
+        new Vector3(30, 0, 0)  // Carril 3
+    };
+
     void Start()
     {
         Spawn();
@@ -15,8 +21,16 @@ public class spawner : MonoBehaviour
 
     void Spawn()
     {
-        int r = Random.Range(0, obsPref.Count);
-        Instantiate(obsPref[r], transform.position, transform.rotation);
+        // Generar hasta 2 objetos
+        for (int i = 0; i < 2; i++)
+        {
+            int r = Random.Range(0, obsPref.Length);
+            int laneIndex = Random.Range(0, lanes.Length);
+            Vector3 spawnPosition = lanes[laneIndex] + transform.position; // Asegúrate de que sea delante del spawner
+
+            Instantiate(obsPref[r], spawnPosition, Quaternion.identity);
+        }
+
         Invoke("Spawn", timeToSpawn);
     }
 }
